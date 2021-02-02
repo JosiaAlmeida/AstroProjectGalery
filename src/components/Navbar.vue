930375905c2d93c2c802c81445cb6ba32bc815b8
Josia <josiassmith005@gmail.com>
2021-02-02 17:43:58 +0100

    First Commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">Inicio</b-nav-item>
+        <b-nav-item href="#">Galeria</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+<style scoped>
+navbar {
+  z-index: 1 !important;
+  position: absolute;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="Home">
+    <Navbar />
+    <div class="container-fluid">
+      <div class="row">
+        <section class="col-12 home">
+          <div class="row">
+            <div class="col-12 ">
+              <h1 class="TextHomeTitulo">Astro Galeria</h1>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar";
+
+export default {
+  name: "Home",
+  components: {
+    Navbar,
+  },
+};
+</script>
+<style scoped>
+.home {
+  position: relative;
+  height: 90vh;
+}
+.home::before {
+  content: "";
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: url("../assets/imgs/img10.jpg") fixed;
+  background-size: cover;
+  filter: brightness(.8) opacity(.9);
+}
+.TextHomeTitulo{
+  text-align: center;
+  color:rgb(220, 228, 227);
+  margin:100px auto !important;
+  font-size: 70px;
+}
+</style>
